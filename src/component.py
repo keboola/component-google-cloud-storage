bd598ba1464bed50dd7261204831c8c0fe1cb52c
Adam Bako <bakoad@gmail.com>
2021-04-22 16:45:00 +0200

    updated to use google client library

--- src/component.py
+++ src/component.py
@@ -3,23 +3,24 @@ Template Component main class.
 
 '''
 import logging
-import mimetypes
 import os
 from datetime import datetime
 from pathlib import Path
 from keboola.component import CommonInterface
-from keboola.http_client import HttpClient
+from google.cloud import storage
+from google.auth.transport import requests
+from google.oauth2.credentials import Credentials as ClientIdCredentials
+from google.oauth2.service_account import Credentials as ServiceCredentials
 
 KEY_BUCKET_NAME = "bucket_name"
 KEY_CLIENT_ID = "appKey"
 KEY_CLIENT_SECRET = "appSecret"
 KEY_REFRESH_TOKEN = "refresh_token"
+KEY_AUTH_DATA = "data"
 KEY_APPENDDATE = "append_date"
+KEY_SERVICE_ACCOUNT = "#service_account_key"
 
-STORAGE_UPLOAD_URL = "https://storage.googleapis.com/upload/storage/v1/b/"
-STORAGE_UPLOAD_ENDPOINT = "o?"
-
-ACCESS_TOKEN_REQUEST_URL = "https://oauth2.googleapis.com/"
+CLIENT_ID_TOKEN_URI = "https://accounts.google.com/o/oauth2/token"
 
 # list of mandatory parameters => if some is missing,
 # component will fail with readable message on initialization.
@@ -42,12 +43,9 @@ def get_data_folder_path():
 
 class Component(CommonInterface):
     def __init__(self):
-        # for easier local project setup
         data_folder_path = get_data_folder_path()
         super().__init__(data_folder_path=data_folder_path)
-
         try:
-            # validation of required parameters. Produces ValueError
             self.validate_configuration(REQUIRED_PARAMETERS)
             self.validate_image_parameters(REQUIRED_IMAGE_PARS)
         except ValueError as e:
@@ -60,17 +58,19 @@ class Component(CommonInterface):
         '''
         params = self.configuration.parameters
 
-        oauth_credentials = self.configuration.oauth_credentials
-        access_token = self._fetch_access_token(oauth_credentials)
+        service_account_credentials = params.get(KEY_SERVICE_ACCOUNT)
+        client_id_credentials = self.configuration.oauth_credentials
+
+        append_date = params[KEY_APPENDDATE]
 
         bucket_name = params.get(KEY_BUCKET_NAME)
-        base_url = STORAGE_UPLOAD_URL + bucket_name
-        http_client = HttpClient(base_url)
+        storage_client = StorageClient.get_storage_client(bucket_name,
+                                                          service_account_credentials=service_account_credentials,
+                                                          client_id_credentials=client_id_credentials)
 
         files_and_tables = self.get_files_and_tables()
-
         for file in files_and_tables:
-            self._upload_file(file, http_client, access_token)
+            self.upload_file(storage_client, bucket_name, file, append_date)
 
     def get_files_and_tables(self):
         in_tables = self.get_input_tables_definitions()
@@ -78,47 +78,63 @@ class Component(CommonInterface):
         in_files = [item for sublist in in_files_per_tag.values() for item in sublist]
         return in_tables + in_files
 
-    def _upload_file(self, file, client, access_token):
-        file_name = self._get_file_name(file.name)
-        post_params = {"uploadType": "media", "name": file_name}
-        content_type = self._get_content_type(file.full_path)
-        post_headers = {"Authorization": access_token, "Content-type": content_type}
+    def upload_file(self, storage_client, bucket_name, file, append_date):
+        source_file_path = file.full_path
+        destination_blob_name = self._get_file_destination_name(file.name, append_date)
+        self._upload_blob(storage_client, bucket_name, source_file_path, destination_blob_name)
 
-        with open(file.full_path, "rb") as a_file:
-            file_dict = {file_name: a_file}
-            response = client.post(STORAGE_UPLOAD_ENDPOINT, files=file_dict, headers=post_headers, params=post_params)
-            logging.info(f"Uploaded {file.full_path} to {response['bucket']}")
-
-    def _get_file_name(self, file_path):
-        params = self.configuration.parameters
+    @staticmethod
+    def _get_file_destination_name(file_path, append_date):
         timestamp_suffix = ''
-        if params[KEY_APPENDDATE]:
+        if append_date:
             timestamp_suffix = "_" + str(datetime.utcnow().strftime('%Y%m%d%H%M%S'))
-
         file_name, file_extension = os.path.splitext(os.path.basename(file_path))
         new_file_name = file_name + timestamp_suffix + file_extension
         return new_file_name
 
     @staticmethod
-    def _get_content_type(file_name):
-        return mimetypes.guess_type(file_name, strict=True)[0]
+    def _upload_blob(storage_client, bucket_name, source_file_path, destination_blob_name):
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(source_file_path)
+
+        logging.info(f"File {source_file_path} uploaded to {destination_blob_name}.")
+
+
+class StorageClient:
 
     @staticmethod
-    def _fetch_access_token(oauth):
-        access_cl = HttpClient(ACCESS_TOKEN_REQUEST_URL)
-        access_token_body = {
-            "client_id": oauth[KEY_CLIENT_ID],
-            "grant_type": "refresh_token",
-            "client_secret": oauth[KEY_CLIENT_SECRET],
-            "refresh_token": oauth["data"][KEY_REFRESH_TOKEN]
-        }
-        access_token_headers = {
-            'content-type': 'application/x-www-form-urlencoded'
-        }
-        response = access_cl.post("token", headers=access_token_headers, data=access_token_body)
+    def get_storage_client(bucket_name, client_id_credentials=None, service_account_credentials=None):
 
-        access_token = "Bearer " + response["access_token"]
-        return access_token
+        if service_account_credentials:
+            storage_client = StorageClient._get_service_account_storage_client(service_account_credentials)
+
+        elif client_id_credentials:
+            client_id = client_id_credentials[KEY_CLIENT_ID]
+            client_secret = client_id_credentials[KEY_CLIENT_SECRET]
+            refresh_token = client_id_credentials[KEY_AUTH_DATA][KEY_REFRESH_TOKEN]
+            storage_client = StorageClient._get_client_id_storage_client(client_id, client_secret, refresh_token,
+                                                                         bucket_name)
+        return storage_client
+
+    @staticmethod
+    def _get_client_id_storage_client(client_id, client_secret, refresh_token, bucket_name):
+        credentials = ClientIdCredentials(None, client_id=client_id,
+                                          client_secret=client_secret,
+                                          refresh_token=refresh_token,
+                                          token_uri=CLIENT_ID_TOKEN_URI)
+        request = requests.Request()
+        credentials.refresh(request)
+        storage_client = storage.Client(credentials=credentials, project=bucket_name)
+        return storage_client
+
+    @staticmethod
+    def _get_service_account_storage_client(service_account_credentials):
+        credentials = ServiceCredentials.from_service_account_info(service_account_credentials)
+        storage_client = storage.Client(credentials=credentials, project=service_account_credentials["project_id"])
+        logging.info(f"Uploading to Google Cloud Storage using {service_account_credentials['client_email']} "
+                     f"service account")
+        return storage_client
 
 
 """
