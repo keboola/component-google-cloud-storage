e84e74c4519a13e7621dee0eba3ec38d75ba8967
Adam Bako <bakoad@gmail.com>
2022-08-03 11:17:00 +0200

    add folder name option

--- src/component.py
+++ src/component.py
@@ -10,10 +10,12 @@ from pathlib import Path
 from keboola.component import CommonInterface
 from google_cloud_storage.client import StorageClient
 from google.auth.exceptions import GoogleAuthError
+from google.api_core.exceptions import NotFound
 
 KEY_BUCKET_NAME = "bucket_name"
 KEY_APPENDDATE = "append_date"
 KEY_SERVICE_ACCOUNT = "#service_account_key"
+KEY_FOLDER_NAME = "folder_name"
 
 REQUIRED_PARAMETERS = [KEY_BUCKET_NAME]
 REQUIRED_IMAGE_PARS = []
@@ -41,7 +43,7 @@ class Component(CommonInterface):
         data_folder_path = get_data_folder_path()
         super().__init__(data_folder_path=data_folder_path)
         try:
-            self.validate_configuration(REQUIRED_PARAMETERS)
+            self.validate_configuration_parameters(REQUIRED_PARAMETERS)
             self.validate_image_parameters(REQUIRED_IMAGE_PARS)
         except ValueError as e:
             logging.exception(e)
@@ -55,6 +57,10 @@ class Component(CommonInterface):
         service_account_json_key = params.get(KEY_SERVICE_ACCOUNT)
         client_id_credentials = self.configuration.oauth_credentials
         bucket_name = params.get(KEY_BUCKET_NAME)
+        folder_name = params.get(KEY_FOLDER_NAME, "")
+        if folder_name and folder_name[-1] != "/":
+            folder_name = f"{folder_name}/"
+
         if service_account_json_key:
             service_account_json_key = KeyCredentials(service_account_json_key).key
 
@@ -68,7 +74,7 @@ class Component(CommonInterface):
         files_and_tables = self.get_files_and_tables()
         append_date = params[KEY_APPENDDATE]
         for file in files_and_tables:
-            self.upload_file(storage_client, bucket_name, file, append_date)
+            self.upload_file(storage_client, bucket_name, folder_name, file, append_date)
 
     def get_files_and_tables(self):
         in_tables = self.get_input_tables_definitions()
@@ -76,21 +82,25 @@ class Component(CommonInterface):
         in_files = [item for sublist in in_files_per_tag.values() for item in sublist]
         return in_tables + in_files
 
-    def upload_file(self, storage_client, bucket_name, file, append_date):
+    def upload_file(self, storage_client, bucket_name, folder_name, file, append_date):
         try:
             source_file_path = file.full_path
-            destination_blob_name = self._get_file_destination_name(file.name, append_date)
+            destination_blob_name = self._get_file_destination_name(folder_name, file.name, append_date)
             storage_client.upload_blob(bucket_name, source_file_path, destination_blob_name)
         except GoogleAuthError as google_error:
             raise UserException(f"Upload failed after retries due to : {google_error}")
+        except NotFound as e:
+            raise UserException(f"Not Found error occurred, make sure Project and Folder in Google cloud exists: {e}")
+        except ValueError as e:
+            raise UserException(e) from e
 
     @staticmethod
-    def _get_file_destination_name(file_path, append_date):
+    def _get_file_destination_name(folder_name, file_path, append_date):
         timestamp_suffix = ''
         if append_date:
             timestamp_suffix = "_" + str(datetime.utcnow().strftime('%Y%m%d%H%M%S'))
         file_name, file_extension = os.path.splitext(os.path.basename(file_path))
-        new_file_name = file_name + timestamp_suffix + file_extension
+        new_file_name = "".join([folder_name, file_name, timestamp_suffix, file_extension])
         return new_file_name
 
 
